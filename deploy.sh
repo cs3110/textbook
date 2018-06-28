2e9d126e4f88105463e261fa08d54fa4cfdad45e
Michael Clarkson <clarksmr@gmail.com>
2018-06-28 15:44:44 -0400

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rsync -rlhcv --delete _book/ cslinux:/people/mrc26/3110-2018fa/textbook
+
+#cslinux:/courses/cs1380/2018sp/textbook
+
+if [[ `git status --porcelain` ]]; then 
+  cat <<EOF
+===========================================================
+WARNING
+
+Your working tree has uncommitted changes, which means 
+you've just deployed something into production that is not
+under source control.  You might want to fix that.  Have 
+you forgotten to pull, to add new files, or to push your 
+changes?  
+
+Run "git status" if you want to see what has changed
+in your working tree.
+===========================================================
+EOF
+fi
